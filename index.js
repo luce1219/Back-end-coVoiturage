0d1c962bbb0bb075c029c25be81d7e7a96de491d
luce1219 <madjinoumounguenguifirmine@gmail.com>
2024-02-06 16:55:14 +0100

    modification

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
  */
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Bonjour le monde' });
+  res.json({ message: 'Bonjour lucette' });
 });
 
 
